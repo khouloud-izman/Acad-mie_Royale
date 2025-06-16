4f2c5ac8799cf792450da3b3adc7ce2561782446
khouloud-izman <izman.khouloud.solicode@gmail.com>
2025-06-16 12:26:33 +0100

    Suppression fichiers supplémentaires

--- assets/js/script3.js
+++
@@ -1,7 +0,0 @@
-function showVideo(index) {
-  const video = document.getElementById('video-' + index);
-  const btn = video.previousElementSibling.querySelector('.video-btn');
-
-  video.classList.add('active');
-  btn.style.display = 'none';
-}
